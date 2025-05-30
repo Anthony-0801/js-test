340999e2994d4ddad3be718720551f5fc5b25cd7
Anthony-0801 <zyxanthony15@gmail.com>
2025-05-30 09:24:17 +0800

    feat: add NaN checks in pow function and update tests with chai

--- index.js
+++ index.js
@@ -10,11 +10,20 @@ app.listen(3000, () => {
 export function pow(x, n) {
   let result = 1;
 
+  if(n < 0) return NaN;
+  if(Math.round(n) != n) return NaN;
+
   for (let i = 0; i < n; i++) {
     result *= x;
   }
-  console.log(result);
+  //console.log(result);
   return result;
 }
+
+// to know the difference between ? and ??
+let x = 10;
+console.log(x === 10 ? "yes they are equal" : "no");
+console.log("There is a data: " + x ?? "There is no data, its null");
+
 
 pow(2, 3); // 8
--- test/test.js
+++ test/test.js
@@ -1,16 +1,16 @@
 import { pow } from "../index.js";
-import assert from "assert";
+import { assert } from "chai";
 
 describe("pow", function () {
-    before(() => {
-        console.log("Starting tests for pow function");
-    })
+  before(() => {
+    console.log("Starting tests for pow function");
+  });
 
   describe("raises a variable x to the power of 3", () => {
     function makeTest(x) {
       let expected = x * x * x;
       it(`${x} in the power 3 is ${expected}`, function () {
-        assert.equal(pow(x, 3), expected, 'bwesit');
+        assert.equal(pow(x, 3), expected, "bwesit error");
       });
     }
 
@@ -19,8 +19,17 @@ describe("pow", function () {
     }
   });
 
+  describe("input that the result must be NaN", () => {
+    it("for negative n the result is NaN", function () {
+      assert.isNaN(pow(2, -1));
+    });
+
+    it("for non-integer n the result is NaN", function () {
+      assert.isNaN(pow(2, 1.5));
+    });
+  });
+
   after(() => {
     console.log("All tests for pow function completed");
   });
-
 });
